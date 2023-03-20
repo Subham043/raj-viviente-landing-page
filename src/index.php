97ce9fee00a225f41227cb3d69bf31720369f5c9
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-03-20 12:58:34 +0530

    fixed banner size

--- src/index.php
+++ src/index.php
@@ -31,12 +31,77 @@
             </div>
         </nav>
     </header>
-    <section class="mb-sm-0">
-        <div class="banner-holder">
+    <section class="">
+        <div class="banner-holder2">
             <img src="assets/rajhighgardens/images/snn-viviente-banners-6.jpg" alt="">
+            <div class="banner-form-container">
+                <div id="tophighlights">
+
+                    <div id="topform" class="formbackground" style="background-color: #F7F2EC;background-image: unset;">
+
+                        <div
+                            style="margin-bottom:10px !important;    border-bottom: 2px solid #1C1919;padding-bottom:8px;padding-top:0px;">
+                            <h1 class="mb8 text-center" style="color:#1C1919;"><b>Raj Viviente by SNN Corp @ 3.9Cr*</b>
+                            </h1>
+                            <h1 class="mb16 text-center" style="    line-height:24px;color:#1C1919;"><b
+                                    style="font-size:14px;    font-weight: 400;">Located Off Bannerghatta Road - South
+                                    Bangalore</b></h1>
+                        </div>
+                        <ul>
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> Ultra Luxury 4BHK(G+2)
+                                Villas + Pvt. Garden</li>
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> 3891 Sqft Villas Just
+                                Off Bannerghatta Rd</li>
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> 5 Mins To Royal
+                                Meenakshi Mall & Decathlon</li>
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> 10 Mins From IIM, Fortis
+                                & Apollo Hospitals</li>
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> 64 Exclusive G+2 Villas
+                                Across 5.5 Acres</li>
+
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> Ultra Modern Clubhouse &
+                                20+ Amenities</li>
+
+                            <li style="font-weight:700;"><span style="color: #3D833E;">➤</span> Double Height Ceiling &
+                                18 Ft. Tall Windows</li>
+
+                            <li><span style="color: #3D833E;">➤</span>Project By The Prestigious SNN Raj Corp</li>
+                        </ul>
+
+
+
+                        <p
+                            style="padding: 5px;font-size:0.8rem;text-align:center;background-color:#3D833E;border-radius: 5px;color:#fff;">
+                            Download Brochure & Floorplans<br><span class='ctanow'><b style="color:#fff;">*Register for
+                                    Special Offers Now*</b></span></p>
+
+                        <form id="banner-form" class="row" method="post">
+                            <div class="col-lg-12 col-md-12 col-sm-12 mb-3">
+                                <input type="text" class="form-control" id="name2" name="name"
+                                    placeholder="Enter your Name">
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 mb-3">
+                                <input type="email" class="form-control" id="email2" name="email"
+                                    aria-describedby="emailHelp" placeholder="Enter your email">
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12 mb-3">
+                                <input type="number" class="form-control" id="phone2" name="phone"
+                                    placeholder="Enter your phone">
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary formbuttonstyler"
+                                    id="submitBtn2">Submit</button>
+                            </div>
+                        </form>
+
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     </section>
-    <section class="mb-sm-0">
+    <!-- <section class="mb-sm-0">
         <div class="banner-holder2">
             <div class="banner-form-container">
                 <div id="tophighlights">
@@ -104,7 +169,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
     <section>
         <div class="container">
             <div class="about-holder" id="about-section">
